2468e697dd2f6324b7f776ead82ab460a78600a4
Cristina <cristina.colon+git@gmail.com>
2015-09-09 13:02:12 -0400

    improvements to chart

--- browser/app/graph/graph.directive.js
+++ browser/app/graph/graph.directive.js
@@ -1,5 +1,10 @@
 app.directive('graph',function() {
   return {
-    templateUrl: '/browser/app/graph/graph.html'
+    templateUrl: '/browser/app/graph/graph.html',
+    controller: function ($scope,AccountFactory) {
+    	AccountFactory.getAccount().then(function(account){
+    		$scope.user = account.id;
+    	});
+    }
   };
 });
